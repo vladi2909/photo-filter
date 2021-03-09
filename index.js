d0a659957ce008c1ba13c3ff17cacacde3bf6fc8
vladimir <vladi2909@outlook.com>
2021-03-09 22:59:13 +0300

    feat: add active buttons

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fullScreenBtn = document.querySelector('.fullscreen').addEventListener('click', toggleScreen);
 
+const btnsContainer = document.querySelector('.btn-container').addEventListener('click', e => toggleBtns(e));
+const btns = document.querySelectorAll('.btn');
+const btnLoad = document.querySelector('.btn-load');
+
 function toggleScreen() {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
@@ -8,4 +12,21 @@ function toggleScreen() {
             document.exitFullscreen();
         }
     }
+}
+
+function toggleBtns(e) {
+    if (e.target.classList.contains('btn')) {
+        btns.forEach(btn => {
+            if (btn.classList.contains('btn-active')) {
+                btn.classList.remove('btn-active');
+            }
+        });
+
+        e.target.classList.add('btn-active');
+    }
+
+    if (e.target.classList.value === 'btn-load--input') {
+        btns.forEach(btn => btn.classList.remove('btn-active'));
+        btnLoad.classList.add('btn-active');
+    } 
 }
