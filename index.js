bf6512878a15d2848b01743d26c53191faff8e68
vladimir <vladi2909@outlook.com>
2021-03-09 15:28:11 +0300

    feat: add fullscreen

---
+++ index.js
@@ -0,0 +1,11 @@
+const fullScreenBtn = document.querySelector('.fullscreen').addEventListener('click', toggleScreen);
+
+function toggleScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.fullscreenEnabled) {
+            document.exitFullscreen();
+        }
+    }
+}
