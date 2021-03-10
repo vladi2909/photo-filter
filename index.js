e4c71a3d2da5a59248d44ae04df60d62b374d5fb
vladimir <vladi2909@outlook.com>
2021-03-10 13:11:09 +0300

    feat: add filters

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const btnsContainer = document.querySelector('.btn-container').addEventListener(
 const btns = document.querySelectorAll('.btn');
 const btnLoad = document.querySelector('.btn-load');
 
+const filters = document.querySelectorAll('.filters input');
+
+
 function toggleScreen() {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
@@ -28,5 +31,34 @@ function toggleBtns(e) {
     if (e.target.classList.value === 'btn-load--input') {
         btns.forEach(btn => btn.classList.remove('btn-active'));
         btnLoad.classList.add('btn-active');
-    } 
+    }
 }
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const outputs = document.querySelectorAll('output');
+
+    switch (this.name) {
+        case 'blur':
+            outputs[0].innerHTML = this.value;
+            break;
+        case 'invert':
+            outputs[1].innerHTML = this.value;
+            break;
+        case 'sepia':
+            outputs[2].innerHTML = this.value;
+            break;
+        case 'saturate':
+            outputs[3].innerHTML = this.value;
+            break;
+        case 'hue':
+            outputs[4].innerHTML = this.value;
+            break;
+        default:
+            break;
+    }
+
+}
+
+filters.forEach(input => input.addEventListener('input', handleUpdate));
